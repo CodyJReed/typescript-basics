7dc7ad9bfbf005214f393e05388e856cf50d540d
CodyJReed <cody2j2reed@gmail.com>
2020-01-20 10:12:32 -0500

    Additional work with interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -1,18 +1,26 @@
-interface Vehicle {
-  name: string;
-  year: number;
-  broken: boolean;
+interface Reportable {
+  summary(): string;
 }
 
 const oldCivic = {
   name: "civic",
   year: 2000,
-  broken: true
+  broken: true,
+  summary(): string {
+    return `Name is ${this.name}`;
+  }
 };
 
-const printVehicle = (vehicle: Vehicle): void =>
-  console.log(
-    `Name: ${vehicle.name}, Year: ${vehicle.year}, Broken: ${vehicle.broken} `
-  );
+const drink = {
+  color: "green",
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar.`;
+  }
+};
 
-printVehicle(oldCivic);
+const printSummary = (item: Reportable): void => console.log(item.summary());
+
+printSummary(oldCivic);
+printSummary(drink);
